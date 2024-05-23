d60dfc689d97956ceafb3c9177cca3833889cb5a
Gideon Horn <gideon.h99@gmail.com>
2024-05-23 16:24:11 +0000

    Add: Event model_Migrated to Admin. Base, event and user HTML starting pages.

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Event_listing
 
-# Register your models here.
+admin.site.register(Event_listing)
--- event/models.py
+++ event/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Let's Go!"))
+
+
+class Event_listing(models.Model):
+    """Models for event details"""
+    event_title = models.CharField(max_length=70, unique=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(default="", null=False)
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+    venue = models.CharField(max_length=100)
+    bio = models.TextField(max_length=500)
+    status = models.IntegerField(choices=STATUS, default=1)
+
+    def __str__(self):
+        return f"{self.event_title}"
+
+    
