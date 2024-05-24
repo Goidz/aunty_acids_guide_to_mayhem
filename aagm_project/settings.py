41a422e705cb645ae0b303ec3a601a46372517f5
Gideon Horn <gideon.h99@gmail.com>
2024-05-24 15:19:20 +0000

    Install-> Cloudinary

--- aagm_project/settings.py
+++ aagm_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'django_summernote',
+    "cloudinary",
     'user',
     'event',
 ]
--- event/views.py
+++ event/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-from django.shortcuts import reverse
+from django.shortcuts import render, reverse, get_object_or_404
 from django.http import HttpResponse
 from django.views import generic
 from event.models import Event_listing
