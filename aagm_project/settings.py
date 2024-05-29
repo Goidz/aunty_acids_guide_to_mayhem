593e14ec07423dee7cf18c78b3f900e3e215d2f2
Gideon Horn <gideon.h99@gmail.com>
2024-05-29 13:54:40 +0000

    Basic hero image etc.

--- aagm_project/settings.py
+++ aagm_project/settings.py
@@ -135,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK='bootstrap5'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Default primary key field type
--- accounts/views.py
+++ accounts/views.py
@@ -2,17 +2,19 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login
-
-
+from django.db import IntegrityError
 
 def signup(request):
     if request.method == "GET":
         return render(request, "signup.html", {"form": UserCreationForm})
     else:
-        if request.POST["password1"] == ["password2"]:
-            user = User.objects.create_user(request.POST["username"], password=request.POST["password1"])
-            user.save()
-            login(request,user)
-            return redirect("home")
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.create_user(request.POST["username"], password=request.POST["password1"])
+                user.save()
+                login(request,user)
+                return redirect("home")
+            except IntegrityError:
+                return render(request, "signup.html", {"form": UserCreationForm, "error": "User already exists"})
         else:
             return render(request, "signup.html", {"form": UserCreationForm, "error": "Passwords do not match, please try again."})
