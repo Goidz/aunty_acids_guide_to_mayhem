e12e608ffc229a831182b349c633c3fa75981d16
Gideon Horn <gideon.h99@gmail.com>
2024-06-12 12:49:06 +0000

    static files

--- aagm_project/settings.py
+++ aagm_project/settings.py
@@ -155,6 +155,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK='bootstrap5'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
